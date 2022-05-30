10ab1a850979e6af9a3aab916095a21687cc23a5
Arlabunakty <durniev.denys@gmail.com>
2022-05-30 14:55:05 +0300

    Initial commit

---
+++ packages/default/findAll/index.js
@@ -0,0 +1,51 @@
+const connect = require('./mongodb-client');
+const { StatusCodes, getReasonPhrase } = require('http-status-codes');
+
+async function main(args) {
+  try {
+    // const userId = args.__ow_headers['x-user-id'];
+    // if (!userId) {
+    //   return {
+    //     error: {
+    //       statusCode: StatusCodes.UNAUTHORIZED,
+    //       body: {
+    //         message: 'user id header required'
+    //       }
+    //     }
+    //   }
+    // }
+
+    const client = await connect();
+    const db = await client.db(process.env.DB_DATABASE);
+    const stationsCollection = await db.collection('stations');
+    const stations = await stationsCollection.find().toArray();
+
+    return {
+      body: {
+        stations
+      }
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      error: {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        body: {
+          message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
+        }
+      }
+    }
+  }
+}
+
+exports.main = main;
+// const qrcode = require('qrcode')
+
+// exports.main = (args) => {
+//   return qrcode.toDataURL(args.text).then(res => ({
+//     headers:  { 'content-type': 'text/html; charset=UTF-8' },
+//     body: args.img == undefined ? res : `<img src="${res}">`
+//   }))
+// }
+
+// if (process.env.TEST) exports.main({text:"hello"}).then(console.log)
---
+++ web/assets/js/index.js
@@ -0,0 +1,83 @@
+function callAjax(url, callback) {
+    var xmlhttp;
+    // compatible with IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            callback(JSON.parse(xmlhttp.responseText));
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function filter(collection, predicate) {
+    var result = new Array();
+    var length = collection.length;
+
+    for (var j = 0; j < length; j++) {
+        if (predicate(collection[j]) == true) {
+            return collection[j];
+        }
+    }
+    return null;
+}
+
+function initMap() {
+    const kiev = { lat: 50.45466, lng: 30.5238 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: kiev,
+    });
+
+    const api = $('body').data('url');
+
+    callAjax(api, (stations) => {
+        const infowindow = new google.maps.InfoWindow({
+            content: "N/A",
+        });
+        for (const station of stations) {
+            if (
+                station.fuelLimits.some(e => e.fuel.description.includes('95') && (e.limitType === 'BANK_CARD'
+                    || e.limitType === 'CASH' || e.limitType === 'MOBILE_APP'))
+            ) {
+                const description = filter(station.fuelLimits, limit => limit.fuel.description.includes('95')
+                    && (limit.limitType === 'BANK_CARD' || limit.limitType === 'CASH' || limit.limitType === 'MOBILE_APP')).description;
+                const marker = new google.maps.Marker({
+                    position: {
+                        lat: station.geoPoint.lat,
+                        lng: station.geoPoint.lon
+                    },
+                    label: '95',
+                    map: map,
+                });
+
+                marker.addListener("click", () => {
+                    infowindow.setContent(description);
+                    infowindow.open({
+                        anchor: marker,
+                        map,
+                        shouldFocus: false,
+                    });
+                });
+            } else {
+                let url = "https://maps.google.com/mapfiles/ms/icons/";
+                url += 'purple' + "-dot.png";
+
+                const marker = new google.maps.Marker({
+                    position: {
+                        lat: station.geoPoint.lat,
+                        lng: station.geoPoint.lon
+                    }, map: map,
+                    icon:
+                    {
+                        url: url,
+                        labelOrigin: new google.maps.Point(60, 30)
+                    }
+                });
+            }
+        }
+    });
+}
+
+window.initMap = initMap;
